0caf1990f8c54feaf7f902a639593567fc451578
Accendit <martijnremmen@hotmail.nl>
2021-09-26 21:39:02 +0200

    Added WIP Q agent

--- server/main.py
+++ server/main.py
@@ -1,35 +1,104 @@
 import logging
 
-from environment import SuperMarioBrosEnvironment
+import gym
+import tensorflow as tf
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import py_driver
+from tf_agents.environments import suite_gym, tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import sequential
+from tf_agents.policies import py_tf_eager_policy, random_tf_policy
+from tf_agents.replay_buffers import reverb_replay_buffer, reverb_utils
+from tf_agents.specs import tensor_spec
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+
+from tf_agents.networks import q_network
+
+import environment
 
 logging.basicConfig(level=logging.INFO)
 
 
+def dense_layer(num_units):
+    return tf.keras.layers.Dense(
+        num_units,
+        activation=tf.keras.activations.relu,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(
+            scale=2.0, mode='fan_in', distribution='truncated_normal'))
+
+
 def main():
-    # time_step = tf_env.reset()
-    # rewards = []
-    # steps = []
-    # num_episodes = 10000
 
-    # for _ in range(num_episodes):
-    #     episode_reward = 0
-    #     episode_steps = 0
-    #     tf_env.reset()
-    #     while not tf_env.current_time_step().is_last():
-    #         action = tf.random_uniform([1], 0, 9, dtype=tf.int32)
-    #         next_time_step = tf_env.step(action)
-    #         episode_steps += 1
-    #         episode_reward += next_time_step.reward.numpy()
-    #     rewards.append(episode_reward)
-    #     steps.append(episode_steps)
+    # num_iterations = 20000 # @param {type:"integer"}
 
-    env = SuperMarioBrosEnvironment()
-    env.serve()
+    # initial_collect_steps = 100  # @param {type:"integer"}
+    # collect_steps_per_iteration =   1# @param {type:"integer"}
+    # replay_buffer_max_length = 100000  # @param {type:"integer"}
 
-    while True:
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        if done: env.reset()
+    # batch_size = 64  # @param {type:"integer"}
+    learning_rate = 1e-3  # @param {type:"number"}
+    # log_interval = 200  # @param {type:"integer"}
+
+    # num_eval_episodes = 10  # @param {type:"integer"}
+    # eval_interval = 1000  # @param {type:"integer"}
+
+    train_env = suite_gym.load('SMB-v0')
+
+    # # env = tf_py_environment.TFPyEnvironment(gym)
+
+    # fc_layer_params = (100, 50)
+    # action_tensor_spec = tensor_spec.from_spec(env.action_spec())
+    # num_actions = 20
+
+
+    # dense_layers = [dense_layer(num_units) for num_units in fc_layer_params]
+    # q_values_layer = tf.keras.layers.Dense(
+    #     num_actions,
+    #     activation=None,
+    #     kernel_initializer=tf.keras.initializers.RandomUniform(
+    #         minval=-0.03, maxval=0.03),
+    #     bias_initializer=tf.keras.initializers.Constant(-0.2))
+    # q_net = sequential.Sequential(dense_layers + [q_values_layer])
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+    # train_step_counter = tf.Variable(0)
+
+    # agent = dqn_agent.DqnAgent(
+    #     env.time_step_spec(),
+    #     env.action_spec(),
+    #     q_network=q_net,
+    #     optimizer=optimizer,
+    #     td_errors_loss_fn=common.element_wise_squared_loss,
+    #     train_step_counter=train_step_counter)
+
+
+    # TODO: QNetwork ondersteund geen multidimensionale actions
+    # moeten network.Sequential gebruiken
+    # https://www.tensorflow.org/agents/api_docs/python/tf_agents/networks/Sequential
+    q_net = q_network.QNetwork(
+        train_env.observation_spec(),
+        train_env.action_spec(),
+        fc_layer_params=(100,))
+
+    agent = dqn_agent.DqnAgent(
+        train_env.time_step_spec(),
+        train_env.action_spec(),
+        q_network=q_net,
+        optimizer=optimizer,
+        td_errors_loss_fn=common.element_wise_squared_loss,
+        train_step_counter=tf.Variable(0))
+
+
+    agent.initialize()
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
