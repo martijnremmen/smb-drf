cbd080fe6cf0671a518abd41972ee3c998d49da2
Accendit <martijnremmen@hotmail.nl>
2021-09-27 21:31:07 +0200

    rllib testing

--- server/main.py
+++ server/main.py
@@ -1,34 +1,23 @@
 import logging
 
 import gym
-import tensorflow as tf
-from tf_agents.agents.dqn import dqn_agent
-from tf_agents.drivers import py_driver
-from tf_agents.environments import suite_gym, tf_py_environment
-from tf_agents.eval import metric_utils
-from tf_agents.metrics import tf_metrics
-from tf_agents.networks import sequential
-from tf_agents.policies import py_tf_eager_policy, random_tf_policy
-from tf_agents.replay_buffers import reverb_replay_buffer, reverb_utils
-from tf_agents.specs import tensor_spec
-from tf_agents.trajectories import trajectory
-from tf_agents.utils import common
+from ray import tune
+from ray.rllib.agents.ppo import PPOTrainer
 
 from environment import SuperMarioBrosEnvironment
 
 logging.basicConfig(level=logging.INFO)
 
 
-def dense_layer(num_units):
-    return tf.keras.layers.Dense(
-        num_units,
-        activation=tf.keras.activations.relu,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(
-            scale=2.0, mode='fan_in', distribution='truncated_normal'))
-
 
 def main():
     gym.register('SMB-v0', entry_point=SuperMarioBrosEnvironment)
+    tune.register_env('SMB-v0', lambda cfg: SuperMarioBrosEnvironment())
+    tune.run(PPOTrainer, config={
+        "env": "SMB-v0",
+        "num_workers": 1,
+        "framework": "tf2"
+        })
 
 
 
