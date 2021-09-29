814973930c4604d3358615268b6f91f829e85134
TimCRN <tim.crounse@gmail.com>
2021-09-29 23:46:45 +0200

    Code removal

--- server/main.py
+++ server/main.py
@@ -6,25 +6,8 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    # time_step = tf_env.reset()
-    # rewards = []
-    # steps = []
-    # num_episodes = 10000
-
-    # for _ in range(num_episodes):
-    #     episode_reward = 0
-    #     episode_steps = 0
-    #     tf_env.reset()
-    #     while not tf_env.current_time_step().is_last():
-    #         action = tf.random_uniform([1], 0, 9, dtype=tf.int32)
-    #         next_time_step = tf_env.step(action)
-    #         episode_steps += 1
-    #         episode_reward += next_time_step.reward.numpy()
-    #     rewards.append(episode_reward)
-    #     steps.append(episode_steps)
 
     env = SuperMarioBrosEnvironment()
-    env.serve()
 
     while True:
         action = env.action_space.sample()
